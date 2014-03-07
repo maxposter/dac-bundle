ce01f0fea110564b22ad715da4c630dd2c50ae93
Svel <Svel.Sontz@gmail.com>
2014-03-07 23:41:57 +0400

    [Annotation] задавать роль, для которой(-ых) не осуществляется фильтрация

--- Annotations/Mapping/Filter.php
+++ Annotations/Mapping/Filter.php
@@ -5,7 +5,12 @@ use Doctrine\ORM\Mapping\Annotation;
 
 /**
  * Dac - аннотации
- * синтаксис: @NS\Filter(targetEntity="Some\Class\Name")
+ * синтаксис: @NS\Filter(targetEntity="Some\Class\Name", role="ROLE_ADMIN")
+ * - targetEntity можно не указывать, если есть связь
+ * - role определяет администратора, для которого фильтры работают иначе:
+ *      * в выборках (SELECT) доступно всё
+ *      * при INSERT/UPDATE/DELETE чужие значения не заменяются
+ *        на "свои", но подставляются если не заданы
  *
  * @Annotation
  * @Target({"PROPERTY"})
@@ -14,4 +19,7 @@ final class Filter implements Annotation
 {
     /** @var string */
     public $targetEntity;
+
+    /** @var array<string> */
+    public $role;
 }
